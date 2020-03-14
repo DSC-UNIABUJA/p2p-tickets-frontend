103ae9d13041819656957d976c1a05b03a685042
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-03-14 22:30:59 +0100

    Transfered rules to prettier file

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	semi: false,
+	trailingComma: "all",
+	printWidth: 100,
+	tabWidth: 4,
+	useTabs: true,
+	singleQuote: false,
+	bracketSpacing: false,
+	jsXSingleQuote: false,
+}
